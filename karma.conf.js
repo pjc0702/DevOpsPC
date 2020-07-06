802eae48d955404d28f7ed83da39005b06367f6e
pj0702 <pjcastit@pronaca.com>
2020-07-06 12:21:00 -0500

    add changes DevOps

--- karma.conf.js
+++ karma.conf.js
@@ -1,32 +1,55 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
-module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-chrome-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular-devkit/build-angular/plugins/karma')
-    ],
-    client: {
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/DevOpsPC'),
-      reports: ['html', 'lcovonly', 'text-summary'],
-      fixWebpackSourcePaths: true
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
-  });
+module.exports = function(config) {
+    config.set({
+        basePath: '',
+        frameworks: ['jasmine', '@angular-devkit/build-angular'],
+        plugins: [
+            require('karma-jasmine'),
+            require('karma-chrome-launcher'),
+            require('karma-jasmine-html-reporter'),
+            require('karma-coverage-istanbul-reporter'),
+            require('@angular-devkit/build-angular/plugins/karma'),
+            require('karma-junit-reporter')
+        ],
+        client: {
+            clearContext: false // leave Jasmine Spec Runner output visible in browser
+        },
+        coverageIstanbulReporter: {
+            dir: require('path').join(__dirname, 'coverage'),
+            reports: ['html', 'lcovonly'],
+            fixWebpackSourcePaths: true
+        },
+        angularCli: {
+            environment: 'dev'
+        },
+        reporters: ['progress', 'kjhtml', 'junit'],
+        junitReporter: {
+            outputDir: "testResults",
+            outputFile: 'test_out/test-results.xml',
+            suite: 'unit',
+            useBrowserName: false
+        },
+        port: 9876,
+        colors: true,
+        logLevel: config.LOG_INFO,
+        autoWatch: true,
+        customLaunchers: {
+            ChromeHeadless: {
+                base: 'Chrome',
+                flags: [
+                        '--headless',
+                        '--disable-gpu',
+                        '--no-sandbox',
+                        '--remote-debugging-port=9222',
+                    ]
+                    // We must disable the Chrome sandbox when running Chrome inside Docker (Chrome's sandbox needs
+                    // more permissions than Docker allows by default)
+                    //flags: isDocker ? ['--no-sandbox'] : []
+            },
+        },
+        browsers: ['ChromeHeadless'],
+        //browsers: ['Chrome'],
+        singleRun: true
+    });
 };
